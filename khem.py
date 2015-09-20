723936dafa32563de0039b06f20a5ecd831f1939
kyuf <jeff.jl.wen@gmail.com>
2015-09-20 08:49:31 -0700

    added mass and mole calculators

--- khem.py
+++ khem.py
@@ -6,11 +6,14 @@ def main():
     
     print("Welcome to khem")
     print("Select from options:\n")
-    print("1. Calculate MW of compound\n")
+    print("1. Calculate MW of compound")
+    print("2. Find mass given moles")
+    print("3. Find moles given mass\n")
+    
     select = input("Enter choice (number):\n>> ")
     
     #available options
-    options = {1}
+    options = {1, 2, 3}
     
     while True:
         try:
@@ -28,6 +31,14 @@ def main():
     if select == 1:
         import find_mw
         find_mw.main()
+    
+    elif select == 2:
+        import calc_mass_and_moles
+        calc_mass_and_moles.main(0)
+    
+    elif select == 3:
+        import calc_mass_and_moles
+        calc_mass_and_moles.main(1)
 
 while True:    
     main()

cbca0cba4cd449edde9e569bc9f156f97186e3be
kyuf <jeff.jl.wen@gmail.com>
2015-09-29 04:20:28 -0700

    added mole balance check. updated to use Species and SpecList classes

--- rxn.py
+++ rxn.py
@@ -1,12 +1,12 @@
-from k_fun import is_number, to_int
+from k_fun import *
 
 #rxn parser
 def parse(rxn):
-    #dicionaries store compound as key and prefix as value
-    reactants = {}
-    products = {}
+    #lists store parsed species
+    reactants = SpecList("Reactants")
+    products = SpecList("Products")
     prefix = ""
-    compound = ""
+    form = ""
     
     #variable to show which side of the equation we are on
     left = True
@@ -20,59 +20,65 @@ def parse(rxn):
         if rxn[i] in special:
             #space will not trigger any action
             if rxn[i] != " ":
-                #error if + or > with empty compound
-                if not compound:
+                #error if + or > with empty form
+                if not form:
                     raise SyntaxError("Improper syntax")
                 elif rxn[i] == "+":
                     #add to reactants/products depending on side
+                    prefix = to_int(prefix)
                     if left:
-                        reactants[compound] = to_int(prefix)
+                        reactants.append(Species(form, prefix))
                     else:
-                        products[compound] = to_int(prefix)
+                        products.append(Species(form, prefix))
                 elif rxn[i] == ">":
                     #add to reactants if on left side
                     if left:
                         left = False
-                        reactants[compound] = to_int(prefix)
+                        reactants.append(Species(form, to_int(prefix)))
                     #error if on right side
                     else:
                         raise SyntaxError("> in wrong location")
                 
-                #reset compound and prefix
-                compound = ""
+                #reset form and prefix
+                form = ""
                 prefix = ""
             i += 1
         
         #append numbers to prefix
         elif is_number(rxn[i]):
-            #error if prefix before compound reset
-            if compound:
+            #error if prefix before form reset
+            if form:
                 raise SyntaxError("Misplaced prefix")
             prefix += rxn[i]
             i += 1
         
-        #capital letter means new compound
+        #capital letter means new form
         elif rxn[i] == rxn[i].upper():
-            #error if compound before compound reset
-            if compound:
+            #error if form before form reset
+            if form:
                 raise SyntaxError("Extra spaces")
-            #continue adding to compound until hitting special character
+            #continue adding to form until hitting special character
             while i < len(rxn) and rxn[i] not in special:
-                compound += rxn[i]
+                form += rxn[i]
                 i += 1
     
-    #add last compound to products, error if compound empty
-    if compound == "":
+    #add last form to products, error if form empty
+    if form == "":
         raise SyntaxError("Missing product")
     else:
-        products[compound] = to_int(prefix)
+        products.append(Species(form, to_int(prefix)))
     
     #error if empty reactants
     if not reactants:
         raise SyntaxError("Missing reactants")
     
+    #check for mole balance
+    if reactants.moles.balance != products.moles.balance:
+        raise SyntaxError("Moles unbalanced")
+    
     return reactants, products
+    
 
-print(parse("2H2 + O2 > 2H2O"))
-print(parse("2H2 + O2 > 2H2O"))
-
+r, p = parse("2H2 + O2 > 2H2O")
+print(r)
+print(p)

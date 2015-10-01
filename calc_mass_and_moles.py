389b5a3b7e2967b591eabca4a4145f45daa9b07e
kyuf <jeff.jl.wen@gmail.com>
2015-10-01 01:16:59 -0700

    updated calculations to use Species class

--- calc_mass_and_moles.py
+++ calc_mass_and_moles.py
@@ -1,5 +1,5 @@
 #calculate either the mass or moles of a compound given the other
-from find_mw import find_mw
+from k_fun import *
 
 def main(select):
     title = ["Mass", "Mole"]
@@ -7,17 +7,17 @@ def main(select):
     print("%s Calculator\n" % (title[select]))
     
     choice = ["moles", "mass [g]"]
-    unit = ["g", " moles"]
+    unit = [" g", " moles"]
     
     #continuous loop
     while True:
         #prompt compound input
         while True:
-            compound = input("Enter compound (Enter 0 to return to khem menu) >> ")
-            if compound == "0":
+            spec = input("Enter species (Enter 0 to return to khem menu) >> ")
+            if spec == "0":
                 return
             try:
-                MW = find_mw(compound)
+                spec = Species(spec)
                 break
             except:
                 print("\nInvalid input\n")
@@ -28,10 +28,10 @@ def main(select):
             try:
                 i = float(i)
                 if select == 0:
-                    calc = i * MW
+                    calc = i * spec.mw
                 else:
-                    calc = i / MW
-                print("\nThere are %s%s in %s%s of %s\n" % (calc, unit[select], i, unit[-select - 1], compound))
+                    calc = i / spec.mw
+                print("\nThere are %.3f%s in %s%s of %s\n" % (calc, unit[select], i, unit[-select - 1], spec.form))
                 break
             except:
                 print("\nInvalid input\n")
